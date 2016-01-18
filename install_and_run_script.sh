42c735c9d50f05635ebb52b438f2bf3f34a1f90e
Sergej Kasper <sergejkasper@gmail.com>
2016-01-18 10:46:45 +0100

    Update install_and_run_script.sh

--- install_and_run_script.sh
+++ install_and_run_script.sh
@@ -2,6 +2,7 @@
 
 git clone https://github.com/aurelia/skeleton-navigation /tmp/aurelia-skeleton
 cp /tmp/aurelia-skeleton/skeleton-es2016 /src
+cd /src
 npm install -y
 npm install gulp
 jspm install -y
